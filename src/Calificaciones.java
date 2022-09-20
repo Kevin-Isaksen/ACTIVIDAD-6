1255cfc37ccb210b996f846ac3f78fe34ea42870
Kevin-Isaksen <al02893309@tecmilenio.mx>
2022-09-20 09:21:58 -0500

    Se agrega Actividad 6

---
+++ src/Calificaciones.java
@@ -0,0 +1,57 @@
+import java.util.Scanner;
+
+public class Calificaciones {
+    static String nombre;
+    static float [] calificaciones = new float[5];
+    static float promedio = 0;
+    static String calificacion = null;
+
+    public static void main(String[] args){
+        Scanner entrada = new Scanner(System.in);
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("Ingrese su nombre");
+        nombre = teclado.nextLine();
+        for (int i = 0; i < 5; i++){
+            System.out.println("Digite la calificación " + (i + 1) + " : ");
+            calificaciones [i] = entrada.nextFloat();
+        }
+        calcular_promedio(calificaciones);
+        calificacion_final(promedio);
+        imprimir(nombre, calificaciones, promedio, calificacion);
+    }
+
+    public static float calcular_promedio (float [] calificaciones){
+        float total = 0;
+        for (int i = 0; i < 5; i++){
+            total = calificaciones [i] + total;
+        }
+        promedio = total / 5;
+        return promedio;
+    }
+
+    public static String calificacion_final (float promedio){
+        if (promedio <= 50){
+            calificacion = "F";
+        }else if (promedio >= 50 && promedio < 61){
+            calificacion = "E";
+        }else if (promedio >= 61 && promedio < 71){
+            calificacion = "D";
+        }else if (promedio >= 71 && promedio < 81){
+            calificacion = "C";
+        }else if (promedio >= 81 && promedio < 91){
+            calificacion = "B";
+        }else if (promedio <= 100){
+            calificacion = "A";
+        }
+        return calificacion;
+    }
+
+    public static void imprimir (String nombre, float [] calificaciones, float promedio, String calificacion){
+        System.out.println("Nombre del estudiante: " + nombre);
+        for (int i = 0; i < 5; i++){
+            System.out.println("Calificación " + (i + 1) + ": " + calificaciones [i]);
+        }
+        System.out.println("Promedio: " + promedio);
+        System.out.println("Calificación: " + calificacion);
+    }
+}
